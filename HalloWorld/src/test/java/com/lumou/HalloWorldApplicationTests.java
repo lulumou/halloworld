49b44254b7076fe27c4a5c1e189407282fcde2b8
lumou <997447204@qq.com>
2022-08-10 15:01:57 +0800

    commit8101500

---
+++ HalloWorld/src/test/java/com/lumou/HalloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
